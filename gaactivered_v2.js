e86e3e5ad2aae98799b16ce8257a13b2b1f5aa3b
tuongnguyen <tuongnguyen3012@gmail.com>
2019-05-08 12:52:56 +0700

    update link

--- gaactivered_v2.js
+++ gaactivered_v2.js
@@ -24,24 +24,20 @@ function getCookie(cname) {
 $(document).ready(function(){
     var list_url = [
         'https://beautynews.top/vitamin-e-and-miraculous-effects-in-skin-beauty-anti-aging/',
-        'https://beautynews.top/vitamin-d-and-the-importance-in-the-beauty-process/',
         'https://beautynews.top/treating-dry-cough-effectively-with-10-daily-dishes/',
-        'https://beautynews.top/is-breakfast-really-the-most-important-meal-of-the-day/',
         'https://beautynews.top/the-5-best-foods-for-knee-pain-patients-must-eat-2019/',
-        'https://beautynews.top/vitamin-c-not-only-helps-you-stay-healthy/',
         'https://beautynews.top/5-vitamins-blow-away-acne-millions-of-people-use-it/',
         'https://beautynews.top/prevent-skin-aging-to-have-forever-20-years-old-skin/',
         'https://beautynews.top/warning-the-anti-vaccine-movement-threatens-childrens-life-dangers-like-hiv/',
         'https://beautynews.top/use-antibiotics-for-children-right-and-absolutely-safe-with-7-principles/',
         'https://beautynews.top/the-standard-way-to-help-people-with-stroke/',
-        'https://beautynews.top/identify-to-distinguish-anal-anus-from-hemorrhoids/',
         'https://beautynews.top/gynaecological-disease-the-fungus-or-mild-inflammation/',
         'https://beautynews.top/drugs-to-prevent-and-treat-sore-throat-sore-throat-effectively/',
         'https://beautynews.top/cure-chronic-throat-inflammation/',
         'https://beautynews.top/is-there-any-way-to-make-your-hair-soft-smooth-and-tangled/',
         'https://beautynews.top/natural-curly-hair-and-treatment/',
         'https://beautynews.top/top-5-best-health-monitoring-wristbands/',
-        'https://beautynews.top/discovering-smart-sports-bracelets-gear-fit2/'
+        'https://beautynews.top/top-list-of-10-best-manual-camera-apps-for-iphone/'
     ];
     var rand = list_url[Math.floor(Math.random() * list_url.length)];
     var history1 = getCookie("history1");
