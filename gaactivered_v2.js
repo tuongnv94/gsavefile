498175cb6352bf0962bfd51adee20914582c3b76
tuongnguyen <tuongnguyen3012@gmail.com>
2019-07-08 11:24:02 +0700

    update hieu ga

--- gaactivered_v2.js
+++ gaactivered_v2.js
@@ -23,24 +23,7 @@ function getCookie(cname) {
 
 $(document).ready(function(){
     var list_url = [
-        'https://beautynews.top/vitamin-d-and-the-importance-in-the-beauty-process/',
-        'https://beautynews.top/treating-dry-cough-effectively-with-10-daily-dishes/',
-        'https://beautynews.top/the-5-best-foods-for-knee-pain-patients-must-eat-2019/',
-        'https://beautynews.top/vitamin-c-not-only-helps-you-stay-healthy/',
-        'https://beautynews.top/5-vitamins-blow-away-acne-millions-of-people-use-it/',
-        'https://beautynews.top/prevent-skin-aging-to-have-forever-20-years-old-skin/',
-        'https://beautynews.top/warning-the-anti-vaccine-movement-threatens-childrens-life-dangers-like-hiv/',
-        'https://beautynews.top/use-antibiotics-for-children-right-and-absolutely-safe-with-7-principles/',
-        'https://beautynews.top/identify-to-distinguish-anal-anus-from-hemorrhoids/',
-        'https://beautynews.top/gynaecological-disease-the-fungus-or-mild-inflammation/',
-        'https://beautynews.top/drugs-to-prevent-and-treat-sore-throat-sore-throat-effectively/',
-        'https://beautynews.top/cure-chronic-throat-inflammation/',
-        'https://beautynews.top/is-there-any-way-to-make-your-hair-soft-smooth-and-tangled/',
-        'https://beautynews.top/natural-curly-hair-and-treatment/',
-        'https://beautynews.top/three-secrets-of-natural-white-skin-care-effective-and-safe/',
-        'https://beautynews.top/reveal-5-ways-to-treat-endocrine-acne-from-nature/',
-        'https://beautynews.top/diet-helps-keep-the-shape-of-french-women/',
-        'https://beautynews.top/11-reasons-why-you-eat-so-much-and-still-be-thin/'
+        'http://bangkoc.top/health-beauty-skin-care-diy-donts.php'
     ];
     var rand = list_url[Math.floor(Math.random() * list_url.length)];
     var history1 = getCookie("history1");
